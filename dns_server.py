cfabf22183e2d0e0784132e6aca29377fc19a58d
akash-suresh <akashsuresh36@gmail.com>
2017-04-06 14:55:32 +0530

    Added basic skeleton of DNS_server code

--- dns_server.py
+++ dns_server.py
@@ -1,1 +1,16 @@
- 
+ import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+
+#define dictionary here!
+ip_addr = 8.8.8.8
+
+while(1):
+	url = socket.recv_string()
+
+	#here the url needs to be matched with an IP and replied back.
+
+	socket.send_string(ip_addr)	
