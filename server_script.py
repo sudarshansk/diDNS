dc50a25355bfc942992ca15d9c0550b805eec17d
Sudarshan <sudarshansk14@gmail.com>
2017-04-10 00:12:01 +0530

    Added working server script

--- server_script.py
+++ server_script.py
@@ -1,4 +1,4 @@
-import dns_server as dns
+import os
 
 servers = [1200,1300]
 ip_addr_default = '8.8.8.8'
@@ -8,8 +8,17 @@ dns_table = {}
 dns_table.update({'default': ip_addr_default, 'www.google.com': ip_addr_google})
 
 serv_num = input('\nEnter number of servers to run\n')
-
+dns_path='laddi'
 servers = servers[:serv_num]
+a = ' '
+for port in servers:
+	a = a + str(port) + ' '
+print a
+server_paths=[]
+for port in servers:
+	cmd = "python dns_server.py "+str(port)+" "+dns_path+a
+	boo = "gnome-terminal -x sh -c \"xdotool getactivewindow; "+cmd+"; exit; bash\""
+	os.system(boo)
 
 
-	
+#"gnome-terminal -x sh -c \"xdotool getactivewindow windowminimize; aplay draw.wav; exit; bash\""
