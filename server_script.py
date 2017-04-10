d6a3cb3db275dcc63dc9bc28f869b4620044bce5
Sudarshan <sudarshansk14@gmail.com>
2017-04-10 12:21:38 +0530

    Final error fixes

--- server_script.py
+++ server_script.py
@@ -1,14 +1,16 @@
 import os
 
-servers = [1200,1300]
+servers = [1200,1300,1400,1500,1600,1700,1800]
 ip_addr_default = '8.8.8.8'
 ip_addr_google = '1.2.3.4'
 
 dns_table = {}
 dns_table.update({'default': ip_addr_default, 'www.google.com': ip_addr_google})
 
-serv_num = input('\nEnter number of servers to run\n')
+serv_num = input('\nEnter number of servers to run(1-7)\n')
+
 servers = servers[:serv_num]
+
 a=' '
 for port in servers:
 	a = a + str(port) + ' '
